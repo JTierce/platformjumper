bd927b83ffa24b9cad8cc8630de35b1cbf4cd1a1
Haydeen74 <49251083+Haydeen74@users.noreply.github.com>
2021-01-19 09:41:15 -0800

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -16,6 +16,10 @@ namespace myTiles {
     export const tile6 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
 
 }
 // Auto-generated code. Do not edit.
