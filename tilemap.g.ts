923fce51786d1b529f4d30dcd6857374e4d65d87
JTierce <49174354+JTierce@users.noreply.github.com>
2021-01-20 10:58:05 -0600

    Restore '1/19/2021, 12:36:20 PM Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts'

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -22,8 +22,6 @@ namespace myTiles {
     export const tile9 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile11 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile10 = image.ofBuffer(hex``);
 
 }
 // Auto-generated code. Do not edit.
